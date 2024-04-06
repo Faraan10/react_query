8a73ceae888f64f6a1d875e8c04327fa6de1d156
npx <faraanshaik19@gmail.com>
2024-04-06 14:34:27 +0530

    react_query

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+	const [newItemName, setNewItemName] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <h4>task bud</h4>
-      <div className='form-control'>
-        <input
-          type='text '
-          className='form-input'
-          value={newItemName}
-          onChange={(event) => setNewItemName(event.target.value)}
-        />
-        <button type='submit' className='btn'>
-          add task
-        </button>
-      </div>
-    </form>
-  );
+	const result = useMutation();
+	// console.log(result);
+
+	const queryClient = useQueryClient();
+	const { mutate: createTask, isLoading } = useMutation({
+		mutationFn: async (newTask) => customFetch.post("/", { title: newTask }),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["tasks"] });
+			toast.success("Task added");
+			setNewItemName("");
+		},
+		onError: (error) => {
+			toast.error(error.response.data.msg);
+		},
+	});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createTask(newItemName);
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<h4>task bud</h4>
+			<div className="form-control">
+				<input
+					type="text "
+					className="form-input"
+					value={newItemName}
+					onChange={(event) => setNewItemName(event.target.value)}
+				/>
+				<button type="submit" className="btn" disabled={isLoading}>
+					add task
+				</button>
+			</div>
+		</form>
+	);
 };
 export default Form;
--- src/Items.jsx
+++ src/Items.jsx
@@ -3,17 +3,28 @@ import SingleItem from "./SingleItem";
 import customFetch from "./utils";
 const Items = () => {
 	//console.log(taskList);
-	const { isLoading, data } = useQuery({
+
+	const { isLoading, isError, error, data } = useQuery({
 		queryKey: ["tasks"],
 		queryFn: async () => {
 			const { data } = await customFetch.get("/");
 			return data;
 		},
 	});
-
 	if (isLoading) {
 		return <p style={{ marginTop: "1rem" }}>Loading...</p>;
 	}
+
+	// this is react query property isError
+	if (isError) {
+		return <p style={{ marginTop: "1rem" }}>There was an error...</p>;
+	}
+
+	// this is axios property error
+	//console.log(error);
+	// if (error) {
+	// 	return <p style={{ marginTop: "1rem" }}>{error.response.data}</p>;
+	// }
 
 	return (
 		<div className="items">
